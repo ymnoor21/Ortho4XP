a08017bc8b18a4c1d8fbe86f30c02c7747c0edf1
Yamin Noor <ymnoor21@gmail.com>
2018-09-09 12:11:20 -0700

    Add yOrtho4XP_Overlays shortcut only if it doesn't exist already

--- scenary_packs_generator.py
+++ scenary_packs_generator.py
@@ -17,14 +17,24 @@ def createWindowsShortcutDirectory(path, target='', workDir='', icon=''):
 if __name__ == "__main__":
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	tiles_dir = '\\Tiles'
-	custom_scenary_path = "D:\\X-Plane 11\\Custom Scenery"
+	xplane_directory = "D:\\X-Plane 11"
+	custom_scenary = xplane_directory + "\\Custom Scenery"
 
 	tiles_dir_path = dir_path + tiles_dir
 
 	for directory in os.listdir(tiles_dir_path):
-		path = custom_scenary_path + "\\" + directory + ".lnk"
+		path = custom_scenary + "\\" + directory + ".lnk"
 		target = tiles_dir_path + "\\" + directory 
 		my_file = Path(path)
 
 		if not my_file.exists():
 			createWindowsShortcutDirectory(path, target)
+
+	# delete and copy yOrtho4XP_Overlays
+	yOverlay_path = custom_scenary + "\\yOrtho4XP_Overlays.lnk"
+	yOverlay_target = dir_path + "\\yOrtho4XP_Overlays"
+
+	yOverlay_lnk = Path(yOverlay_path)
+
+	if not yOverlay_lnk.exists():
+		createWindowsShortcutDirectory(yOverlay_path, yOverlay_target)
